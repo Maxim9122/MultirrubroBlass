f7ffae8dd6bb75dc3e3d8125b597b76f42669f72
Maxi <110688390+Maxim9122@users.noreply.github.com>
2025-01-21 10:55:42 -0300

    Subida

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/MercaditoJyR/';
+    public $baseURL = 'http://localhost/MultirubroBlass/';
 
     /**
      * --------------------------------------------------------------------------
