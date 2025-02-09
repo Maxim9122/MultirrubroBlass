7cc11c257a31494732033eb439a065f1f17fe9f8
Maxi <110688390+Maxim9122@users.noreply.github.com>
2025-02-09 18:40:55 -0300

    Seguridad

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -156,9 +156,9 @@ $routes->get('turnos/(:num)', 'Carrito_controller::ListaTurnosCabeceraCliente/$1
 $routes->get('factura/(:num)', 'Carrito_controller::FacturaCliente/$1');
 
 //AFIP
-$routes->get('verificarTA', 'Carrito_controller::verificarTA');
-$routes->get('generarTA', 'Carrito_controller::generarTA');
-$routes->get('facturar', 'Carrito_controller::facturar');
+$routes->get('verificarTA/(:num)', 'Carrito_controller::verificarTA/$1');
+//$routes->get('generarTA', 'Carrito_controller::generarTA');
+$routes->get('generarTicket/(:num)', 'Carrito_controller::facturar/$1');
 
 
 /*
