dcaff097f8cc4f2873ea4a33f5309a0a95902fba
NLuisA <luis_elpara@hotmail.com.ar>
2025-03-08 20:52:57 -0300

    se elimino boton producto,carrito y pedido de admin

--- app/Views/navbar/navbar.php
+++ app/Views/navbar/navbar.php
@@ -41,16 +41,6 @@
         <div id="navBar" class="navBar">
             <ul class="navList flex">
         <?php if( ($perfil =='1')) { ?>
-          
-          <li class="nnavItem">
-            <a href="<?= base_url('/catalogo')?>" class="btn">Productos</a>
-          </li>
-          <li class="navItem">
-          <a href="<?php echo base_url('CarritoList') ?>"> <img class="navImg"  src=" <?php echo base_url('assets/img/icons/iconMB2.png')?>"> </a>
-          </li>
-          <li class="nnavItem">
-            <a href="<?= base_url('pedidos')?>" class="btn">PEDIDOS</a>
-          </li>
           <li class="nnavItem">
             <a class="btn signUp" href="<?php echo base_url('compras');?>">VENTAS</a>
           </li>
